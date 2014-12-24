52cc0915b04bd7738e41fa5dca90666789000b9c
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-24 13:47:57 -0500

    listening for messages

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var irc = require('irc')
 		;
 
 var client = new irc.Client('irc.freenode.net', 'SmallBatchBot', { channels: ['#smallbatchdev'] });
+
+client.addListener('message', function (from, to, message) {
+	console.log(from + ' => ' + to + ': ' + message);
+});
